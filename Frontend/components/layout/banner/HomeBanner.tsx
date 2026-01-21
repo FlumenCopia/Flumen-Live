989c77f933308436dd4bd52f3bf58214c9219ac7
arjunvs1 <arjunvs.avs@gmail.com>
2026-01-21 13:42:56 +0530

    updated icons

--- Frontend/components/layout/banner/HomeBanner.tsx
+++ Frontend/components/layout/banner/HomeBanner.tsx
@@ -5,14 +5,16 @@ import BannerImg from "@/public/images/flumenxbanner.webp";
 const HomeBanner = () => {
   return (
     <div style={{ position: 'relative', width: '100%', height: '100vh', overflow: 'hidden' }}>
-      <Image 
-        src={BannerImg} 
-        alt="Banner Image" 
-        fill
-        priority
-        style={{ objectFit: "cover" }}
-        quality={100}
-      />
+      <a href="/about-us">
+        <Image 
+          src={BannerImg} 
+          alt="Banner Image" 
+          fill
+          priority
+          style={{ objectFit: "cover" }}
+          quality={100}
+        />
+      </a>
     </div>
   );
 };
